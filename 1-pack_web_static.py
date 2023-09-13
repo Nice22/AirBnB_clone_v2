d0429e310a170b37bc84fe0000722f0f36f352cb
Nice22 <benothouessou74@gmail.com>
2023-09-13 15:37:53 +0000

    update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,15 +8,13 @@ from datetime import datetime
 import os
 
 def do_pack():
-
     """
     Creates a compressed archive of the web_static folder
     """
     try:
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        local("mkdir -p versions")
         archive_name = "versions/web_static_{}.tgz".format(timestamp)
-        local("tar -cvzf {} web_static".format(archive_name))
+        local("tar -czvf {} web_static".format(archive_name))
         return archive_name
     except:
         return None
