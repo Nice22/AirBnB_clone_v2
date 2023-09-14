aaee20598f6d65ed4da3719deea26d21d7aee3fa
Nice22 <benothouessou74@gmail.com>
2023-09-14 07:25:55 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ Fabric script to execute the do_deploy function
 from fabric.api import *
 import os
 
-env.hosts = ["35.196.96.41", "3.234.218.189"]
+env.hosts = ["54.144.137.166", "54.237.14.236"]
 env.user = "ubuntu"
 env.key_filename = "/root/0-RSA_public_key"
 
@@ -24,13 +24,13 @@ def do_deploy(archive_path):
     remote_folder = "/data/web_static/releases/{}".format(archive_basename)
 
     put(archive_path, "/tmp/")
-    run("mkdir -p {}".format(remote_folder))
-    run("tar -xzf /tmp/{} -C {}/".format(archive_filename, remote_folder))
-    run("rm /tmp/{}".format(archive_filename))
-    run("mv {}/web_static/* {}".format(remote_folder, remote_folder))
-    run("rm -rf {}/web_static".format(remote_folder))
-    run("rm -rf /data/web_static/current")
-    run("ln -s {} /data/web_static/current".format(remote_folder))
+    run("sudo mkdir -p {}".format(remote_folder))
+    run("sudo tar -xzf /tmp/{} -C {}/".format(archive_filename, remote_folder))
+    run("sudo rm /tmp/{}".format(archive_filename))
+    run("sudo mv {}/web_static/* {}".format(remote_folder, remote_folder))
+    run("sudo rm -rf {}/web_static".format(remote_folder))
+    run("sudo rm -rf /data/web_static/current")
+    run("sudo ln -s {} /data/web_static/current".format(remote_folder))
 
     print("New version deployed!")
     return True
