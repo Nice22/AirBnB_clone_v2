89e54ab973e76f4bbdde11de4a763c5019f4bd0e
Nice22 <benothouessou74@gmail.com>
2023-09-14 17:16:59 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,25 +3,28 @@
 Fabric script to execute the do_deploy function
 """
 
-from fabric.api import *
+from fabric.api import env, put, run  # Import put and run functions
 import os
 
 env.hosts = ["54.144.137.166", "54.237.14.236"]
 env.user = "ubuntu"
 env.key_filename = "/root/0-RSA_public_key"
 
+
 def do_deploy(archive_path):
     """
-    Executes the do_deploy function on remote servers
+    Deploy a compressed archive to the web server.
+
+    Args:
+        archive_path (str): The path to the archive to be deployed.
+
+    Returns:
+        bool: True if deployment is successful, False otherwise.
     """
     if not os.path.exists(archive_path):
         print("Archive path does not exist. Aborting deployment.")
         return False
 
-    """
-    Args:
-    """
-    
     archive_filename = os.path.basename(archive_path)
     archive_basename = os.path.splitext(archive_filename)[0]
 
@@ -37,13 +40,10 @@ def do_deploy(archive_path):
     run("sudo ln -s {} /data/web_static/current".format(remote_folder))
 
     print("New version deployed!")
-    
-    """
-    Returns:
-    """
-
     return True
 
+
 if __name__ == "__main__":
-    archive_path = "versions/web_static_20230914071506.tgz"  # Update with your actual archive path
-    do_deploy(archive_pat)
+    archive_path = "versions/web_static_20230914071506.tgz"
+    # Update with your actual archive path
+    do_deploy(archive_path)
